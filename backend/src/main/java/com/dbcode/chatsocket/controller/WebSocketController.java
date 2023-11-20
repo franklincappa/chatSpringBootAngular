17f4e5adc694d67aa0e46e683a9d9032be6ec0f4
Franklin Cappa Ticona <franklincappa@gmail.com>
2023-11-19 21:01:03 -0500

    actualización url cors bk

--- backend/src/main/java/com/dbcode/chatsocket/controller/WebSocketController.java
+++ backend/src/main/java/com/dbcode/chatsocket/controller/WebSocketController.java
@@ -5,12 +5,15 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@CrossOrigin(origins = "http://localhost")
 public class WebSocketController {
     @MessageMapping("/chat/{roomId}")
     @SendTo("/topic/{roomId}")
     public ChatMessage chat(@DestinationVariable String roomId, ChatMessage message){
+        System.out.println(message);
         return new ChatMessage(message.getMessage(), message.getUser());
     }
 }
